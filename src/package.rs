5abb0103b47e7e4d323475c8fea0ad8c11bf2da9
fl3xice <two.fl3xice@yandex.ru>
2024-04-10 10:04:14 +0400

    exclude and include enum type

--- src/package.rs
+++ src/package.rs
@@ -1,10 +1,22 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
+pub enum PackType<T> {
+    Include(T),
+    Exclude,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct DiscordGuildPackage {
     /// Package version
     pub version: u8,
     /// Source guild id
     pub guild_id: u64,
     pub created_at: chrono::DateTime<chrono::Utc>,
+    pub pack: GuildPack,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GuildPack {
+    pub name: PackType<String>,
 }
